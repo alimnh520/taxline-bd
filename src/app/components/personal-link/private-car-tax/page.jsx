d45520b01e638363e948fa7d17f42f568acaf384
alimnh520 <alimnh412@example.com>
2025-11-21 17:45:20 +0600

    add all

---
+++ src/app/components/personal-link/private-car-tax/page.jsx
@@ -0,0 +1,10 @@
+// /components/personal-link/reduced-income-tax/page.js
+import React from "react";
+
+export default function page() {
+    return (
+        <div>
+            <h1>Null page</h1>
+        </div>
+    );
+}
